94fba58d627d50e4ff327c860fc39ffce6fb8fb6
jsfriedman <friedman.jonny@gmail.com>
2024-01-26 20:56:22 -0500

    components and adding buttons

---
+++ src/components/body/Body.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import birdLogo from '../../assets/bird_transparent.png';
+
+const Body:any = () =>{
+    return(
+    <div>
+        <h1>Under Construction</h1>
+        <img src={birdLogo} width="25%" height="25%"></img>
+    </div>
+    )
+}
+export default Body;
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Header.css';
+
+const Header:any = () => {
+    return(
+        <div className="header">
+            <button>Button1</button>
+            <button>Button2</button>
+            <button>Button3</button>
+            <button>Button4</button>
+        </div>
+    )
+}
+export default Header;
