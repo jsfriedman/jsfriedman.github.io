2c86f5e7cdf112c68d767e4645f320e2c58be421
jsfriedman <friedman.jonny@gmail.com>
2024-01-27 21:18:06 -0500

    Styling and settging up classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { useState } from 'react';
 import './App.css';
 import Body from './components/body/Body';
 import Header from './components/header/Header';
 
 
 function App() {
+  const [sectionToDisplay, setSectionToDisplay] = useState<string>("Button1");
   return (
     <>
       <Header/>
--- src/components/body/Body.tsx
+++ src/components/body/Body.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import birdLogo from '../../assets/bird_transparent.png';
+import './Body.css';
 
 const Body:any = () =>{
     return(
-    <div>
+    <div className='centered'>
         <h1>Under Construction</h1>
         <img src={birdLogo} width="25%" height="25%"></img>
     </div>
