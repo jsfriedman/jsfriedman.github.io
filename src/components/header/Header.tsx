78f4ad702ce5d2509470467fcb02e4270853993e
jsfriedman <friedman.jonny@gmail.com>
2024-01-30 21:57:58 -0500

    named buttons

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,10 +4,11 @@ import './Header.css';
 const Header:any = () => {
     return(
         <div className="header">
-            <button>Button1</button>
-            <button>Button2</button>
-            <button>Button3</button>
-            <button>Button4</button>
+            <button>Home</button>
+            <button>About Me</button>
+            <button>Resume</button>
+            <button>Shelfscan</button>
+            <button>Thoughts</button>
         </div>
     )
 }
