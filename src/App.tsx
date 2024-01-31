e4917b71daffe6747d45db7ced2ff8939c067087
jsfriedman <friedman.jonny@gmail.com>
2024-01-31 00:00:33 -0500

    added state for selected button, made projects expandable

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Body from './components/body/Body';
 import Header from './components/header/Header';
 
 
 function App() {
-  const [sectionToDisplay, setSectionToDisplay] = useState<string>("Button1");
-  
+  const [activeSection, setActiveSection] = useState<string>("Home");
+
+  const handleHeaderButtonClick = (section: string) => {
+    setActiveSection(section);
+  }
+
   return (
     <>
-      <Header/>
+      <Header buttonOnClickFn={handleHeaderButtonClick} activeSection={activeSection}/>
       <Body/>
     </>
   )
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { FC } from "react";
 import './Header.css';
 
-const Header:any = () => {
+interface headerProps {
+    buttonOnClickFn: (section:string) => void;
+    activeSection: string;
+};
+
+const Header: FC<headerProps> = ({buttonOnClickFn, activeSection}) => {
+
+    const projects = ['Shelfscan'];
+
     return(
         <div className="header">
-            <button>Home</button>
-            <button>About Me</button>
-            <button>Resume</button>
-            <button>Shelfscan</button>
-            <button>Thoughts</button>
+            <button onClick={() => buttonOnClickFn("Home")}>Home</button>
+            <button onClick={() => buttonOnClickFn("About Me")}>About Me</button>
+            <button onClick={() => buttonOnClickFn("Resume")}>Resume</button>
+            <button onClick={() => buttonOnClickFn("Projects")}>Projects</button>
+            {activeSection==='Projects' && projects.map((project)=>(<button onClick={() => buttonOnClickFn(project)}>{project}</button>))}
+            <button onClick={() => buttonOnClickFn("Thoughts")}>Thoughts</button>
         </div>
     )
 }
