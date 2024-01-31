241fba7c9b8419c9f63ec7df658b36a2165b7347
jsfriedman <friedman.jonny@gmail.com>
2024-01-31 00:50:36 -0500

    compiler config changes

--- src/components/body/Body.tsx
+++ src/components/body/Body.tsx
@@ -6,17 +6,17 @@ interface bodyProps {
     activeSection: string;
 };
 
-const Body:FC<bodyProps> = ({activeSection}) =>{
-    return(
-    <div className='centered'>
-        {activeSection==='Home' &&
-        <>
-            <h1>Under Construction</h1>
-            <img src={birdLogo} width="25%" height="25%"></img>
-        </>
-        }
-        {activeSection && <p>{activeSection}</p>}
-    </div>
+const Body: FC<bodyProps> = ({ activeSection }) => {
+    return (
+        <div className='centered'>
+            {activeSection === 'Home' &&
+                <>
+                    <h1>Under Construction</h1>
+                    <img src={birdLogo} width="25%" height="25%"></img>
+                </>
+            }
+            {activeSection && <p>{activeSection}</p>}
+        </div>
     )
 }
 export default Body;
